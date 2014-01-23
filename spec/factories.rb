caa482e21592280f95bb0e80aade1495243f470a
ForGerita <362896799@qq.com>
2014-01-23 22:19:32 +0800

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,13 @@
 
 FactoryGirl.define do
 	factory :user do
-		name	"Andy Young"
-		email "andyoung@foxmail.com"
-		password "gerita"
-		password_confirmation "gerita"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
+		password "foobar"
+		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,10 @@ require 'spec_helper'
 describe User do
 
 	before do
-		@user = User.new(name: "Example User", email: "user@example.com",
-										password: "foobar", password_confirmation: "foobar")
+		@user = User.new(name: "Example User",
+                    email: "user@example.com",
+                    password: "foobar",
+                    password_confirmation: "foobar")
 	end
 
 	subject { @user }
@@ -17,8 +19,20 @@ describe User do
 	it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
-	it { should be_valid }
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    it { should be_admin }
+
+  end
+
 
 	describe "when name is not present" do
 		before { @user.name = " " }
